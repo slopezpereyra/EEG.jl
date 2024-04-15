6d07bd95c4fcb424d1d0d54522f992ef25c27ba0
santi <santiagolopezpereyra@gmail.com>
2024-04-15 15:12:50 -0300

    Relative spindle power algorithm

--- src/spindles.jl
+++ src/spindles.jl
@@ -25,10 +25,25 @@ function sigma_index(x::Vector{<:AbstractFloat}, fs::Integer)
     end
 
     Σ = map(compute_index, amps)
-    if filter 
-        return 
     return(Σ)
 end
+
+function relative_spindle_power(x::Vector{<:AbstractFloat}, fs::Integer)
+    segs = overlaps(x, fs, 0)
+    amps = map(x -> AmplitudeSpectrum(x, fs, 512), segs)
+    
+    function f(amp::AmplitudeSpectrum, band_lower::Number, band_upper::Number, g::Function)
+        freq_band = findall(x -> x >= band_lower && x <= band_upper, amp.freq)
+        freq_band_power = amp.spectrum[freq_band]
+        g(freq_band_power)
+    end
+
+    spindle_band_powers = map(x -> f(x, 11, 15, sum), amps)
+    total_band_powers = map(x -> f(x, 0.5, 40, sum), amps)
+
+    return spindle_band_powers ./ total_band_powers
+end
+
 
 
 #x = [sin(100 * x * 2 * pi) + 4 * sin(110 *x *2 * pi) for x in 1:1/500:5]
